81243509b94b3839d5846930fe8dbbf55d84c6eb
ForkSort <95124571+ForkSort@users.noreply.github.com>
2022-05-02 14:15:34 +0200

    add fetch to MainScreen

--- react/state_and_effect/src/components/MainScreen.jsx
+++ react/state_and_effect/src/components/MainScreen.jsx
@@ -1,6 +1,27 @@
+import React, { useState, useEffect } from 'react';
 const MainScreen = () => {
+    const [memeimage, setMemeImage] = useState(null);
+    const [fetchError, setFetchError] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await fetch("https://meme-api.herokuapp.com/gimme");
+                const json = await result.json();
+                setMemeImage(json.url)
+            }
+            catch (error) {
+                setFetchError("Error: " + error)
+            }
+        }
+        fetchData()
+    },[memeimage])
+
     return (
-        <div style={{backgroundColor: "black", minWidth: "200px", minHeight: "200px"}}></div>
+        <div style={{backgroundColor: "black", minWidth: "200px", minHeight: "200px"}}>
+            {memeimage && <img src={memeimage}></img>}
+            {fetchError && <p style={{color: "red", fontWeight: "bold", textAlign: "center"}}>{fetchError}</p>}
+        </div>
     );
 }
 export default MainScreen;
